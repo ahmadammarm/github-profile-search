7f1cd38862f87b29446d2c7f851d3cfb0a38d2c0
Ahmad Ammar Musyaffa <ahmad.ammar.2205356@students.um.ac.id>
2023-10-11 20:39:01 +0700

    update loading

---
+++ src/components/Loader.js
@@ -0,0 +1,22 @@
+import React from 'react'
+
+const Loader = () => {
+  return (
+    <div
+    className='
+        flex
+        items-center
+        justify-center
+        flex-col
+        text-gray-400
+        italic
+        px-4
+        py-10
+    '
+    >
+      Loading....
+    </div>
+  )
+}
+
+export default Loader
