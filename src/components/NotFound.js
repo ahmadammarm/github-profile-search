3caafff54f4380aa0f8db37acd6f8eef1830c48b
Ahmad Ammar Musyaffa <ahmad.ammar.2205356@students.um.ac.id>
2023-10-12 14:53:37 +0700

    adding profile not found

---
+++ src/components/NotFound.js
@@ -0,0 +1,21 @@
+import React from 'react'
+
+const NotFound = () => {
+  return (
+    <div className='
+        flex
+        items-center
+        justify-center
+        flex-col
+        text-gray-400
+        italic
+        px-4
+        py-10
+        text-2xl
+    '>
+      Profile Not Found
+    </div>
+  )
+}
+
+export default NotFound
