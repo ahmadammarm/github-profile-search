b3244325a10619f8e0c04fac9b3c0a67c1d3dd91
Ahmad Ammar Musyaffa <113039347+ahmadammarm@users.noreply.github.com>
2023-10-30 17:08:29 +0700

    update footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -12,7 +12,7 @@ const Footer = () => {
         italic
         px-4
     '>
-      <p>&copy; Created by <a href='https://ahmadammarm.github.io/ammar' target='_blank' rel="noopener noreferrer" className='
+      <p>&copy; Created by <a href='https://ammar-porto.vercel.app' target='_blank' rel="noopener noreferrer" className='
       text-gray-500
       hover:text-gray-400
       transition
